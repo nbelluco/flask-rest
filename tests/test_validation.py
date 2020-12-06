d518f6d66a6104e977f616e0a248a2cc45615c87
Nicolò Belluco <nicolobelluco98@gmail.com>
2020-12-06 15:48:01 +0100

    Addede few more unit tests for validate_german_plate

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -3,6 +3,22 @@ import unittest
 from src.validation import validate_german_plate
 
 
+class TestValidPlate(unittest.TestCase):
+
+    # Returns True for plate with max length
+    def test_max_length(self):
+        self.assertTrue(validate_german_plate('MMM-AA1230'))
+        self.assertTrue(validate_german_plate('M-AA1230'))
+        self.assertTrue(validate_german_plate('MMM-A1230'))
+        self.assertTrue(validate_german_plate('ZZZ-ZZ9999'))
+        self.assertTrue(validate_german_plate('M-A1230'))
+
+    # Returns True for plate with min length
+    def test_min_length(self):
+        self.assertTrue(validate_german_plate('M-A1'))
+        self.assertFalse(validate_german_plate('M-A0'))
+
+
 class TestInvalidPlate(unittest.TestCase):
 
     # Returns False for empty string
@@ -12,6 +28,10 @@ class TestInvalidPlate(unittest.TestCase):
     # Returns False for plate containing non-letter characters
     def test_extra_chars(self):
         self.assertFalse(validate_german_plate('M&-PP123'))
+        self.assertFalse(validate_german_plate('M--PP123'))
+        self.assertFalse(validate_german_plate('M-PP?123'))
+        self.assertFalse(validate_german_plate('M-PP123?'))
+        self.assertFalse(validate_german_plate('M-PP123%'))
 
     # Returns False for plate with extra digits
     def test_extra_digits(self):
