ebb3e1255b56fbb6ce65a89f6a3eec635c0802af
Nicolò Belluco <nicolobelluco98@gmail.com>
2020-12-06 16:05:11 +0100

    Removed 'deactive' command in venv-install.sh

--- venv-install.sh
+++ venv-install.sh
@@ -3,7 +3,6 @@ echo "Installing virtualenv in .venv folder"
 python3 -m venv .venv
 source .venv/bin/activate
 pip3 install -r requirements.txt
-deactivate
 
 echo "SUCCESS: virtualenv installed."
 echo "run 'source .venv/bin/activate' to enable it"
